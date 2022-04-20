89a943c9438bcc2bc37912536bcb00d26063ebe6
Ricardo <riicardo.ro@gmail.com>
2022-04-20 11:48:28 -0500

    html avanzado, obtener datos desde formulario

---
+++ generation_html/html_avanzado/assets/js/main.js
@@ -0,0 +1,20 @@
+
+const color= document.querySelector("#inputcolor")
+console.log(color)
+color.addEventListener("change",function(e){
+   document.body.style.backgroundColor=e.target.value;
+})
+
+
+
+const formulario = document.querySelector("#formularioDatos") /* Selecciona el formulario */
+console.log(formulario)
+
+formulario.addEventListener("submit", function(e){
+    e.preventDefault() /* hace que se detenga hasta que reciba los datos */
+    const datos = Object.fromEntries(
+        new FormData(e.target)
+    )|
+    console.log(datos)
+
+})
